6ed3602cc5723785b3c16128042c41cb9feb2e4d
Christopher Kung <christopher.w.kung@nasa.gov>
2025-01-09 08:48:47 -0800

    Editing and verifying translate test for V remapping

--- tests/savepoint/translate/translate_Pressures_mapV.py
+++ tests/savepoint/translate/translate_Pressures_mapV.py
@@ -20,18 +20,18 @@ class TranslatePressures_mapV(TranslateFortranData2Py):
                 "jend": grid.je+1,
                 "kend": grid.npz+1
                 },
-            "pe0_v": {
+            "pe0_": {
                 "istart": grid.is_,
                 "iend": grid.ie+1,
                 "jstart": grid.js,
-                "jend": grid.je,
+                "jend": grid.je+1,
                 "kend": grid.npz+1
             },
-            "pe3_v": {
+            "pe3_": {
                 "istart": grid.is_,
                 "iend": grid.ie+1,
                 "jstart": grid.js,
-                "jend": grid.je,
+                "jend": grid.je+1,
                 "kend": grid.npz+1
             },
             "ak":{
@@ -40,74 +40,74 @@ class TranslatePressures_mapV(TranslateFortranData2Py):
             "bk":{
 
             },
-            # "v_": {
-            #     "istart": grid.isd,
-            #     "iend": grid.ied+1,
-            #     "jstart": grid.jsd,
-            #     "jend": grid.jed,
-            #     "kend": grid.npz-1,
-            # },
+            "v_": {
+                "istart": grid.isd,
+                "iend": grid.ied+1,
+                "jstart": grid.jsd,
+                "jend": grid.jed,
+                "kend": grid.npz-1,
+            },
 
-            # "mfx_": {
-            #     "istart": grid.is_,
-            #     "iend": grid.ie+1,
-            #     "jstart": grid.js,
-            #     "jend": grid.je,
-            #     "kend": grid.npz-1,
-            # },
+            "mfx_": {
+                "istart": grid.is_,
+                "iend": grid.ie+1,
+                "jstart": grid.js,
+                "jend": grid.je,
+                "kend": grid.npz-1,
+            },
 
-            # "cx_": {
-            #     "istart": grid.is_,
-            #     "iend": grid.ie+1,
-            #     "jstart": grid.jsd,
-            #     "jend": grid.jed,
-            #     "kend": grid.npz-1,
-            # },
+            "cx_": {
+                "istart": grid.is_,
+                "iend": grid.ie+1,
+                "jstart": grid.jsd,
+                "jend": grid.jed,
+                "kend": grid.npz-1,
+            },
 
 
         }
         self.in_vars["parameters"] = [
-            # "kord_mt",
+            "kord_mt",
         ]
 
         self.out_vars = {
-            "pe0_v": {
+            "pe0_": {
+                "istart": grid.is_,
+                "iend": grid.ie+1,
+                "jstart": grid.js,
+                "jend": grid.je+1,
+                "kend": grid.npz+1,
+            },
+            "pe3_": {
+                "istart": grid.is_,
+                "iend": grid.ie+1,
+                "jstart": grid.js,
+                "jend": grid.je+1,
+                "kend": grid.npz+1,
+            },
+            "v_": {
+                "istart": grid.isd,
+                "iend": grid.ied+1,
+                "jstart": grid.jsd,
+                "jend": grid.jed,
+                "kend": grid.npz-1,
+            },
+
+            "mfx_": {
                 "istart": grid.is_,
                 "iend": grid.ie+1,
                 "jstart": grid.js,
                 "jend": grid.je,
-                "kend": grid.npz+1,
+                "kend": grid.npz-1,
             },
-            "pe3_v": {
+
+            "cx_": {
                 "istart": grid.is_,
                 "iend": grid.ie+1,
-                "jstart": grid.js,
-                "jend": grid.je,
-                "kend": grid.npz+1,
+                "jstart": grid.jsd,
+                "jend": grid.jed,
+                "kend": grid.npz-1,
             },
-            # "v_": {
-            #     "istart": grid.isd,
-            #     "iend": grid.ied+1,
-            #     "jstart": grid.jsd,
-            #     "jend": grid.jed,
-            #     "kend": grid.npz-1,
-            # },
-
-            # "mfx_": {
-            #     "istart": grid.is_,
-            #     "iend": grid.ie+1,
-            #     "jstart": grid.js,
-            #     "jend": grid.je,
-            #     "kend": grid.npz-1,
-            # },
-
-            # "cx_": {
-            #     "istart": grid.is_,
-            #     "iend": grid.ie+1,
-            #     "jstart": grid.jsd,
-            #     "jend": grid.jed,
-            #     "kend": grid.npz-1,
-            # },
         }
 
         grid_indexing = stencil_factory.grid_indexing
@@ -119,46 +119,46 @@ class TranslatePressures_mapV(TranslateFortranData2Py):
             origin=grid_indexing.origin_compute(),
             domain=(
                 grid_indexing.domain[0] + 1,
-                grid_indexing.domain[1],
+                1,
                 grid_indexing.domain[2] + 1,
             ),
         )
 
     def compute_from_storage(self, inputs):
-        # self._map1_ppm_v = MapSingle(
-        #     self.stencil_factory,
-        #     self.quantity_factory,
-        #     inputs["kord_mt"],
-        #     -1,
-        #     dims=[X_INTERFACE_DIM, Y_DIM, Z_DIM],
-        # )
+        self._map1_ppm_v = MapSingle(
+            self.stencil_factory,
+            self.quantity_factory,
+            inputs["kord_mt"],
+            -1,
+            dims=[X_INTERFACE_DIM, Y_DIM, Z_DIM],
+        )
 
         self._pressures_mapv(
                 inputs["pe_"],
                 inputs["ak"],
                 inputs["bk"],
-                inputs["pe0_v"],
-                inputs["pe3_v"],
+                inputs["pe0_"],
+                inputs["pe3_"],
             )
 
-        # self._map1_ppm_v(
-        #         inputs["v_"],
-        #         inputs["pe0_v"],
-        #         inputs["pe3_v"],
-        #         interp=False,
-        #     )
+        self._map1_ppm_v(
+                inputs["v_"],
+                inputs["pe0_"],
+                inputs["pe3_"],
+                interp=False,
+            )
         
-        # self._map1_ppm_v(
-        #         inputs["mfx_"],
-        #         inputs["pe0_v"],
-        #         inputs["pe3_v"],
-        #         interp=False,
-        #     )
+        self._map1_ppm_v(
+                inputs["mfx_"],
+                inputs["pe0_"],
+                inputs["pe3_"],
+                interp=False,
+            )
         
-        # self._map1_ppm_v(
-        #         inputs["cx_"],
-        #         inputs["pe0_v"],
-        #         inputs["pe3_v"],
-        #         interp=False,
-        #     )
+        self._map1_ppm_v(
+                inputs["cx_"],
+                inputs["pe0_"],
+                inputs["pe3_"],
+                interp=False,
+            )
         return inputs
