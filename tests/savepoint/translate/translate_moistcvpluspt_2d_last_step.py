3c4a5643a5c7a24019428cea9680680dd4af805d
Christopher Kung <christopher.w.kung@nasa.gov>
2025-01-14 13:32:44 -0800

    Remove extraneous code from moist_cv_pt_last_step translate test

--- tests/savepoint/translate/translate_moistcvpluspt_2d_last_step.py
+++ tests/savepoint/translate/translate_moistcvpluspt_2d_last_step.py
@@ -5,90 +5,10 @@ from ndsl.dsl.typing import FloatField, Float
 from ndsl.stencils.testing import TranslateFortranData2Py, pad_field_in_j
 from pyFV3.stencils import moist_cv
 
-
-def moist_pt(
-    qvapor: FloatField,
-    qliquid: FloatField,
-    qrain: FloatField,
-    qsnow: FloatField,
-    qice: FloatField,
-    qgraupel: FloatField,
-    q_con: FloatField,
-    pt: FloatField,
-    cappa: FloatField,
-    delp: FloatField,
-    delz: FloatField,
-    r_vir: Float,
-):
-    with computation(PARALLEL), interval(...):
-        cvm, gz, q_con, cappa, pt = moist_cv.moist_pt_func(
-            qvapor,
-            qliquid,
-            qrain,
-            qsnow,
-            qice,
-            qgraupel,
-            q_con,
-            pt,
-            cappa,
-            delp,
-            delz,
-            r_vir,
-        )
-
-
-class MoistPT:
-    """
-    Class to test with DaCe orchestration. test class is MoistCVPlusPt_2d
-    """
-
-    def __init__(
-        self,
-        stencil_factory: StencilFactory,
-        grid,
-    ):
-        self._moist_cv_pt = stencil_factory.from_origin_domain(
-            moist_pt,
-            origin=grid.compute_origin(),
-            domain=(grid.nic, 1, grid.npz),
-        )
-
-    def __call__(
-        self,
-        qvapor: FloatField,
-        qliquid: FloatField,
-        qrain: FloatField,
-        qsnow: FloatField,
-        qice: FloatField,
-        qgraupel: FloatField,
-        q_con: FloatField,
-        pt: FloatField,
-        cappa: FloatField,
-        delp: FloatField,
-        delz: FloatField,
-        r_vir: float,
-    ):
-        self._moist_cv_pt(
-            qvapor,
-            qliquid,
-            qrain,
-            qsnow,
-            qice,
-            qgraupel,
-            q_con,
-            pt,
-            cappa,
-            delp,
-            delz,
-            r_vir,
-        )
-
-
 class TranslateMoistCVPlusPt_2d_last_step(TranslateFortranData2Py):
     def __init__(self, grid, namelist, stencil_factory):
         super().__init__(grid, namelist, stencil_factory)
         self.stencil_factory = stencil_factory
-        self.compute_func = MoistPT(stencil_factory, self.grid)  # type: ignore
         self.in_vars["data_vars"] = {
             "qvapor": {
                 "kend": grid.npz-1,
@@ -111,11 +31,6 @@ class TranslateMoistCVPlusPt_2d_last_step(TranslateFortranData2Py):
             "pt": {},
             "pkz": {},
         }
-        # self.write_vars = ["qvapor", "qliquid", "qice", "qrain", "qsnow", "qgraupel","qcld"]
-        # for k, v in self.in_vars["data_vars"].items():
-        #     # if k not in self.write_vars:
-        #     if k in self.write_vars:
-        #         v["axis"] = 1
 
         self.in_vars["parameters"] = ["r_vir", "dtmp"]
         self.out_vars = {
@@ -139,13 +54,7 @@ class TranslateMoistCVPlusPt_2d_last_step(TranslateFortranData2Py):
         )
 
     def compute_from_storage(self, inputs):
-        # for name, value in inputs.items():
-        #     if hasattr(value, "shape") and len(value.shape) > 1 and value.shape[1] == 1:
-        #         inputs[name] = self.make_storage_data(
-        #             pad_field_in_j(
-        #                 value, self.grid.njd, backend=self.stencil_factory.backend
-        #             )
-        #         )
+
         self.compute_func(inputs["qvapor"],
                           inputs["qliquid"],
                           inputs["qrain"],
