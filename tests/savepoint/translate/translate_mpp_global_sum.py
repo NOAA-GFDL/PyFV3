18a5218386da17c0b28ea66735be4594b773feeb
Christopher Kung <christopher.w.kung@nasa.gov>
2025-01-29 07:59:36 -0800

    Enabled mpp_global_sum with flag to choose 'precise' or 'simplified' reduction calculation

--- tests/savepoint/translate/translate_mpp_global_sum.py
+++ tests/savepoint/translate/translate_mpp_global_sum.py
@@ -1,14 +1,8 @@
-import numpy as np
-
 from ndsl import Namelist, StencilFactory
-from ndsl.comm.comm_abc import ReductionOperator
-from ndsl.quantity import Quantity
 from ndsl.stencils.testing import ParallelTranslate
 from ndsl.stencils.testing.grid import Grid
 from ndsl.typing import Communicator
-
-
-# from pyFV3.stencils.mpp_global_sum import mpp_global_sum
+from pyFV3.stencils.mpp_global_sum import mpp_global_sum
 
 
 class TranslateMpp_global_sum(ParallelTranslate):
@@ -36,32 +30,10 @@ class TranslateMpp_global_sum(ParallelTranslate):
 
     def compute_parallel(self, inputs, communicator: Communicator):
 
-        # inputs["tesum"] = mpp_global_sum(inputs["inputArray"],
-        # communicator,
-        # self.stencil_factory)
-
-        ints_sum_reduce = Quantity(
-            data=np.zeros(inputs["inputArray"].data.shape, dtype=np.float32),
-            dims=["I", "J"],
-            units="dunno",
-            gt4py_backend=self.stencil_factory.backend,
+        inputs["tesum"] = mpp_global_sum(
+            inputs["inputArray"],
+            communicator,
+            self.stencil_factory,
         )
-
-        inputArray = Quantity(
-            data=inputs["inputArray"].astype(np.float32),
-            dims=["I", "J"],
-            units="dunno",
-            gt4py_backend=self.stencil_factory.backend,
-        )
-
-        # print("ints_sum_reduce shape: ", ints_sum_reduce.data.shape)
-
-        # print("inputs[inputArray] type: ", type(inputs["inputArray"]))
-        # print("ints_sum_reduce type: ", type(ints_sum_reduce))
-
-        communicator.all_reduce(inputArray, ReductionOperator.SUM, ints_sum_reduce)
-
-        inputs["tesum"] = sum(sum(ints_sum_reduce.data))
-        # print("inputs[tesum]: ", inputs["tesum"])
 
         return inputs
