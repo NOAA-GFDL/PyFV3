15ea24402f7411acc813bb1ba49120e87437059e
Janice Kim <janice.kim@noaa.gov>
2025-04-25 15:50:35 -0400

    Mods based on https://github.com/NOAA-GFDL/PyFV3/pull/50 - Use NamelistDefaults.sw_dynamics - Replace assert with raised TypeError in analytic_init.py

--- pyFV3/_config.py
+++ pyFV3/_config.py
@@ -274,7 +274,7 @@ class DynamicalCoreConfig:
     nf_omega: int = NamelistDefaults.nf_omega
     fv_sg_adj: int = NamelistDefaults.fv_sg_adj
     n_sponge: int = NamelistDefaults.n_sponge
-    sw_dynamics: bool = DEFAULT_BOOL
+    sw_dynamics: bool = NamelistDefaults.sw_dynamics
     namelist_override: Optional[str] = None
 
     def __post_init__(self):
