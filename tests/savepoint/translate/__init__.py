8cf966b335584a2d9be8880b5951599a3eff7f37
Florian Deconinck <deconinck.florian@gmail.com>
2025-04-30 15:24:59 -0400

    Clean up Translate test

--- tests/savepoint/translate/__init__.py
+++ tests/savepoint/translate/__init__.py
@@ -92,7 +92,6 @@ from .translate_qsinit import TranslateQSInit
 from .translate_ray_fast import TranslateRay_Fast
 from .translate_remapping import TranslateRemapping
 from .translate_remapping_GEOS import TranslateRemapping_GEOS
-from .translate_remapping_GEOS_v2 import TranslateRemapping_GEOS_v2
 from .translate_riem_solver3 import TranslateRiem_Solver3
 from .translate_riem_solver_c import TranslateRiem_Solver_C
 from .translate_satadjust3d import TranslateSatAdjust3d
