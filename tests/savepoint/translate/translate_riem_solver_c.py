7d5987fb5a30feae237c5c01a077e530dcb01db6
Florian Deconinck <deconinck.florian@gmail.com>
2025-05-02 14:17:16 -0400

    Extend rieman solver test to halos

--- tests/savepoint/translate/translate_riem_solver_c.py
+++ tests/savepoint/translate/translate_riem_solver_c.py
@@ -1,6 +1,9 @@
+import numpy as np
 from ndsl import Namelist, StencilFactory
+from ndsl.constants import X_DIM, Y_DIM, Z_DIM
 from pyFV3.stencils import NonhydrostaticVerticalSolverCGrid
 from pyFV3.testing import TranslateDycoreFortranData2Py
+from pyFV3.utils.functional_validation import get_subset_func
 
 
 class TranslateRiem_Solver_C(TranslateDycoreFortranData2Py):
@@ -31,3 +34,23 @@ class TranslateRiem_Solver_C(TranslateDycoreFortranData2Py):
         self.out_vars = {"pef": {"kend": grid.npz}, "gz": {"kend": grid.npz}}
         self.max_error = 5e-14
         self.stencil_factory = stencil_factory
+        self._subset = get_subset_func(
+            self.grid.grid_indexing,
+            dims=[X_DIM, Y_DIM, Z_DIM],
+            n_halo=((3, 3), (3, 3)),
+        )
+
+    def compute(self, inputs):
+        outputs = super().compute(inputs)
+        outputs["gz"] = self.subset_output("gz", outputs["gz"])
+        return outputs
+
+    def subset_output(self, varname: str, output: np.ndarray) -> np.ndarray:
+        """
+        Given an output array, return the slice of the array which we'd
+        like to validate against reference data
+        """
+        if varname in ["gz", "pef"]:
+            return self._subset(output)
+        else:
+            return output
