d6b82411a485b5e4db1c5ce75234b9728061c50a
Frank Malatino <frank.malatino@noaa.gov>
2025-07-15 12:55:34 -0400

    New linting tools

--- pyfv3/stencils/remap_profile.py
+++ pyfv3/stencils/remap_profile.py
@@ -37,9 +37,7 @@ def constrain_interior(q, gam, a4):
     return (
         limit_both(q, a4)
         if (gam[0, 0, -1] * gam[0, 0, 1] > 0.0)
-        else limit_maxmin(q, a4)
-        if (gam[0, 0, -1] > 0.0)
-        else limit_minmax(q, a4)
+        else limit_maxmin(q, a4) if (gam[0, 0, -1] > 0.0) else limit_minmax(q, a4)
     )
 
 
@@ -402,17 +400,13 @@ def set_interpolation_coefficients(
             tmp_min = (
                 a4_1
                 if (a4_1 < pmp_1) and (a4_1 < lac_1)
-                else pmp_1
-                if pmp_1 < lac_1
-                else lac_1
+                else pmp_1 if pmp_1 < lac_1 else lac_1
             )
             tmp_max0 = a4_2 if a4_2 > tmp_min else tmp_min
             tmp_max = (
                 a4_1
                 if (a4_1 > pmp_1) and (a4_1 > lac_1)
-                else pmp_1
-                if pmp_1 > lac_1
-                else lac_1
+                else pmp_1 if pmp_1 > lac_1 else lac_1
             )
             a4_2 = tmp_max0 if tmp_max0 < tmp_max else tmp_max
             # right edges?
@@ -421,17 +415,13 @@ def set_interpolation_coefficients(
             tmp_min = (
                 a4_1
                 if (a4_1 < pmp_2) and (a4_1 < lac_2)
-                else pmp_2
-                if pmp_2 < lac_2
-                else lac_2
+                else pmp_2 if pmp_2 < lac_2 else lac_2
             )
             tmp_max0 = a4_3 if a4_3 > tmp_min else tmp_min
             tmp_max = (
                 a4_1
                 if (a4_1 > pmp_2) and (a4_1 > lac_2)
-                else pmp_2
-                if pmp_2 > lac_2
-                else lac_2
+                else pmp_2 if pmp_2 > lac_2 else lac_2
             )
             a4_3 = tmp_max0 if tmp_max0 < tmp_max else tmp_max
             a4_4 = 3.0 * (2.0 * a4_1 - (a4_2 + a4_3))
@@ -458,33 +448,25 @@ def set_interpolation_coefficients(
                     tmp_min = (
                         a4_1
                         if (a4_1 < pmp_1) and (a4_1 < lac_1)
-                        else pmp_1
-                        if pmp_1 < lac_1
-                        else lac_1
+                        else pmp_1 if pmp_1 < lac_1 else lac_1
                     )
                     tmp_max0 = a4_2 if a4_2 > tmp_min else tmp_min
                     tmp_max = (
                         a4_1
                         if (a4_1 > pmp_1) and (a4_1 > lac_1)
-                        else pmp_1
-                        if pmp_1 > lac_1
-                        else lac_1
+                        else pmp_1 if pmp_1 > lac_1 else lac_1
                     )
                     a4_2 = tmp_max0 if tmp_max0 < tmp_max else tmp_max
                     tmp_min = (
                         a4_1
                         if (a4_1 < pmp_2) and (a4_1 < lac_2)
-                        else pmp_2
-                        if pmp_2 < lac_2
-                        else lac_2
+                        else pmp_2 if pmp_2 < lac_2 else lac_2
                     )
                     tmp_max0 = a4_3 if a4_3 > tmp_min else tmp_min
                     tmp_max = (
                         a4_1
                         if (a4_1 > pmp_2) and (a4_1 > lac_2)
-                        else pmp_2
-                        if pmp_2 > lac_2
-                        else lac_2
+                        else pmp_2 if pmp_2 > lac_2 else lac_2
                     )
                     a4_3 = tmp_max0 if tmp_max0 < tmp_max else tmp_max
                     a4_4 = 6.0 * a4_1 - 3.0 * (a4_2 + a4_3)
@@ -499,16 +481,12 @@ def set_interpolation_coefficients(
             tmp_min2 = (
                 a4_1
                 if (a4_1 < pmp_1) and (a4_1 < lac_1)
-                else pmp_1
-                if pmp_1 < lac_1
-                else lac_1
+                else pmp_1 if pmp_1 < lac_1 else lac_1
             )
             tmp_max2 = (
                 a4_1
                 if (a4_1 > pmp_1) and (a4_1 > lac_1)
-                else pmp_1
-                if pmp_1 > lac_1
-                else lac_1
+                else pmp_1 if pmp_1 > lac_1 else lac_1
             )
             tmp2 = a4_2 if a4_2 > tmp_min2 else tmp_min2
             tmp_min3 = a4_1 if a4_1 < pmp_2 else pmp_2
