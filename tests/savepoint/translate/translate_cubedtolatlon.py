bd1c3a9773a22553f9d52bc0a91da1f198a04d7a
Janice Kim <janice.kim@noaa.gov>
2025-10-17 10:22:17 -0400

    Adding self.config to TranslateCubedToLatLon

--- tests/savepoint/translate/translate_cubedtolatlon.py
+++ tests/savepoint/translate/translate_cubedtolatlon.py
@@ -4,6 +4,7 @@ from ndsl import Quantity, StencilFactory
 from ndsl.constants import X_DIM, X_INTERFACE_DIM, Y_DIM, Y_INTERFACE_DIM, Z_DIM
 from ndsl.stencils.c2l_ord import CubedToLatLon
 from ndsl.stencils.testing import ParallelTranslate2Py
+from pyfv3 import DynamicalCoreConfig
 
 
 class TranslateCubedToLatLon(ParallelTranslate2Py):
@@ -33,6 +34,7 @@ class TranslateCubedToLatLon(ParallelTranslate2Py):
             "v": self.grid.x3d_domain_dict(),
         }
         self.stencil_factory = stencil_factory
+        self.config = DynamicalCoreConfig.from_f90nml(namelist)
         self.grid_type = self.config.grid_type
 
     def compute_parallel(self, inputs, communicator):
