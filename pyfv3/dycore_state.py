1847679a2d7dd59f2183273764c7d113e98304e6
Roman Cattaneo <1116746+romanc@users.noreply.github.com>
2025-10-29 10:50:48 +0100

    remove unused ignore statements

--- pyfv3/dycore_state.py
+++ pyfv3/dycore_state.py
@@ -345,7 +345,7 @@ class DycoreState:
                     extent=sizer.get_extent(dims),
                     gt4py_backend=backend,
                 )
-        state = cls(**dict_state)  # type: ignore
+        state = cls(**dict_state)
         return state
 
     @classmethod
