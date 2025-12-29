9b1f81871b525f1b42f18e2f4d0c210dfed3d97d
Frank Malatino <frank.malatino@noaa.gov>
2025-12-29 11:06:03 -0500

    Adding in potential halo updater for grav_var

--- pyfv3/stencils/fv_dynamics.py
+++ pyfv3/stencils/fv_dynamics.py
@@ -378,6 +378,9 @@ class DynamicalCore:
         self._omega_halo_updater = WrappedHaloUpdater(
             comm.get_scalar_halo_updater([full_xyz_spec]), state, ["omga"], comm=comm
         )
+        self._gravity_halo_updater = WrappedHaloUpdater(
+            comm.get_scalar_halo_updater([full_xyz_spec], state, ["grav_var"], comm=comm)
+        )
         self._n_split = config.n_split
         self._k_split = config.k_split
         self._conserve_total_energy = config.consv_te
