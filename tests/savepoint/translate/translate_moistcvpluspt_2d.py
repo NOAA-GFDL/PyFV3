d1b54ccdc5c578dd4d4ee3109c35f37f7099cd31
Florian Deconinck <deconinck.florian@gmail.com>
2026-04-13 16:42:11 -0400

    Fix translates

--- tests/savepoint/translate/translate_moistcvpluspt_2d.py
+++ tests/savepoint/translate/translate_moistcvpluspt_2d.py
@@ -86,7 +86,7 @@ class MoistPT:
 
 class TranslateMoistCVPlusPt_2d(TranslateFortranData2Py):
     def __init__(self, grid, namelist, stencil_factory):
-        super().__init__(grid, namelist, stencil_factory)
+        super().__init__(grid, stencil_factory)
         self.stencil_factory = stencil_factory
         self.compute_func = MoistPT(stencil_factory, self.grid)  # type: ignore
         self.in_vars["data_vars"] = {
@@ -118,9 +118,7 @@ class TranslateMoistCVPlusPt_2d(TranslateFortranData2Py):
         for name, value in inputs.items():
             if hasattr(value, "shape") and len(value.shape) > 1 and value.shape[1] == 1:
                 inputs[name] = self.make_storage_data(
-                    pad_field_in_j(
-                        value, self.grid.njd, backend=self.stencil_factory.backend
-                    )
+                    pad_field_in_j(value, self.grid.njd, backend=self.stencil_factory.backend)
                 )
         self.compute_func(**inputs)
         return inputs
