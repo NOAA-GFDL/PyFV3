3cc1b963b2fd46a71c6ad730b932517cc5fb686b
Frank Malatino <frank.malatino@noaa.gov>
2026-04-29 09:41:55 -0400

    Changes before merge of develop 29 April 2026

--- pyfv3/stencils/wam.py
+++ pyfv3/stencils/wam.py
@@ -1,5 +1,5 @@
 from ndsl import StencilFactory
-from ndsl.constants import GRAV, RADIUS, RDGAS, X_DIM, Y_DIM, Z_DIM, Z_INTERFACE_DIM
+from ndsl.constants import GRAV, RADIUS, RDGAS, I_DIM, J_DIM, K_DIM, K_INTERFACE_DIM
 from ndsl.dsl.gt4py import BACKWARD, FORWARD, computation
 from ndsl.dsl.gt4py import function as gtfunction
 from ndsl.dsl.gt4py import interval
