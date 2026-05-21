311bc20d3e51ee3f2c3ecc8ead89c7b92f0b52b1
Tobias Wicky-Pfund <wicky.tob@gmail.com>
2026-05-21 09:19:00 +0200

    corner fix for orchestration

--- pyfv3/stencils/copy_corners.py
+++ pyfv3/stencils/copy_corners.py
@@ -9,28 +9,28 @@ from ndsl.optional_imports import cupy as cp
 
 
 @singledispatch
-def corner_copy_x(field_to_copy):
-    raise NotImplementedError(f"No CopyCorners for type {type(field_to_copy)}")
+def corner_copy_x(output_field, input_field):
+    raise NotImplementedError(f"No CopyCorners for type {type(output_field)}")
 
 
 if cp is not None:
 
     @corner_copy_x.register(cp.ndarray)
-    def _corner_copy_x_cupy(field_to_copy: cp.ndarray):
-        _blind_copy_corners_x(field_to_copy)
+    def _corner_copy_x_cupy(output_field: cp.ndarray, input_field: cp.ndarray):
+        _type_agnostic_copy_corners_x(output_field, input_field)
 
 
 @corner_copy_x.register(np.ndarray)
-def _corner_copy_x_numpy(field_to_copy: np.ndarray):
-    _blind_copy_corners_x(field_to_copy)
+def _corner_copy_x_numpy(output_field: np.ndarray, input_field: np.ndarray):
+    _type_agnostic_copy_corners_x(output_field, input_field)
 
 
 @corner_copy_x.register(Quantity)
-def _corner_copy_x_quantity(field_to_copy: Quantity):
-    _blind_copy_corners_x(field_to_copy.data)
+def _corner_copy_x_quantity(output_field: Quantity, input_field: Quantity):
+    _type_agnostic_copy_corners_x(output_field.data, input_field.data)
 
 
-def _blind_copy_corners_x(field_to_copy):
+def _type_agnostic_copy_corners_x(output_field, input_field):
     """Equivalent to the copy_corners_x functions in fortran.
 
     This is written to operate on plain ndarrarys and not use the GT4Py framework.
@@ -44,78 +44,78 @@ def _blind_copy_corners_x(field_to_copy):
         field_to_copy (ndarray): field to apply the corner copy on.
             This is explicitly not type-hinted for orchestration
     """
-    field_to_copy[0, 0] = field_to_copy[0, 5]
-    field_to_copy[0, 1] = field_to_copy[1, 5]
-    field_to_copy[0, 2] = field_to_copy[2, 5]
+    output_field[0, 0] = input_field[0, 5]
+    output_field[0, 1] = input_field[1, 5]
+    output_field[0, 2] = input_field[2, 5]
 
-    field_to_copy[1, 0] = field_to_copy[0, 4]
-    field_to_copy[1, 1] = field_to_copy[1, 4]
-    field_to_copy[1, 2] = field_to_copy[2, 4]
+    output_field[1, 0] = input_field[0, 4]
+    output_field[1, 1] = input_field[1, 4]
+    output_field[1, 2] = input_field[2, 4]
 
-    field_to_copy[2, 0] = field_to_copy[0, 3]
-    field_to_copy[2, 1] = field_to_copy[1, 3]
-    field_to_copy[2, 2] = field_to_copy[2, 3]
+    output_field[2, 0] = input_field[0, 3]
+    output_field[2, 1] = input_field[1, 3]
+    output_field[2, 2] = input_field[2, 3]
 
-    field_to_copy[0, -4] = field_to_copy[2, -7]
-    field_to_copy[0, -3] = field_to_copy[1, -7]
-    field_to_copy[0, -2] = field_to_copy[0, -7]
+    output_field[0, -4] = input_field[2, -7]
+    output_field[0, -3] = input_field[1, -7]
+    output_field[0, -2] = input_field[0, -7]
 
-    field_to_copy[1, -4] = field_to_copy[2, -6]
-    field_to_copy[1, -3] = field_to_copy[1, -6]
-    field_to_copy[1, -2] = field_to_copy[0, -6]
+    output_field[1, -4] = input_field[2, -6]
+    output_field[1, -3] = input_field[1, -6]
+    output_field[1, -2] = input_field[0, -6]
 
-    field_to_copy[2, -4] = field_to_copy[2, -5]
-    field_to_copy[2, -3] = field_to_copy[1, -5]
-    field_to_copy[2, -2] = field_to_copy[0, -5]
+    output_field[2, -4] = input_field[2, -5]
+    output_field[2, -3] = input_field[1, -5]
+    output_field[2, -2] = input_field[0, -5]
 
-    field_to_copy[-4, 0] = field_to_copy[-2, 3]
-    field_to_copy[-4, 1] = field_to_copy[-3, 3]
-    field_to_copy[-4, 2] = field_to_copy[-4, 3]
+    output_field[-4, 0] = input_field[-2, 3]
+    output_field[-4, 1] = input_field[-3, 3]
+    output_field[-4, 2] = input_field[-4, 3]
 
-    field_to_copy[-3, 0] = field_to_copy[-2, 4]
-    field_to_copy[-3, 1] = field_to_copy[-3, 4]
-    field_to_copy[-3, 2] = field_to_copy[-4, 4]
+    output_field[-3, 0] = input_field[-2, 4]
+    output_field[-3, 1] = input_field[-3, 4]
+    output_field[-3, 2] = input_field[-4, 4]
 
-    field_to_copy[-2, 0] = field_to_copy[-2, 5]
-    field_to_copy[-2, 1] = field_to_copy[-3, 5]
-    field_to_copy[-2, 2] = field_to_copy[-4, 5]
+    output_field[-2, 0] = input_field[-2, 5]
+    output_field[-2, 1] = input_field[-3, 5]
+    output_field[-2, 2] = input_field[-4, 5]
 
-    field_to_copy[-4, -2] = field_to_copy[-2, -5]
-    field_to_copy[-4, -3] = field_to_copy[-3, -5]
-    field_to_copy[-4, -4] = field_to_copy[-4, -5]
+    output_field[-4, -2] = input_field[-2, -5]
+    output_field[-4, -3] = input_field[-3, -5]
+    output_field[-4, -4] = input_field[-4, -5]
 
-    field_to_copy[-3, -2] = field_to_copy[-2, -6]
-    field_to_copy[-3, -3] = field_to_copy[-3, -6]
-    field_to_copy[-3, -4] = field_to_copy[-4, -6]
+    output_field[-3, -2] = input_field[-2, -6]
+    output_field[-3, -3] = input_field[-3, -6]
+    output_field[-3, -4] = input_field[-4, -6]
 
-    field_to_copy[-2, -2] = field_to_copy[-2, -7]
-    field_to_copy[-2, -3] = field_to_copy[-3, -7]
-    field_to_copy[-2, -4] = field_to_copy[-4, -7]
+    output_field[-2, -2] = input_field[-2, -7]
+    output_field[-2, -3] = input_field[-3, -7]
+    output_field[-2, -4] = input_field[-4, -7]
 
 
 @singledispatch
-def corner_copy_y(field_to_copy):
-    raise NotImplementedError(f"No CopyCorners for type {type(field_to_copy)}")
+def corner_copy_y(output_field, input_field):
+    raise NotImplementedError(f"No CopyCorners for type {type(output_field)}")
 
 
 if cp is not None:
 
     @corner_copy_y.register(cp.ndarray)
-    def _corner_copy_y_cupy(field_to_copy: cp.ndarray):
-        _blind_copy_corners_y(field_to_copy)
+    def _corner_copy_y_cupy(output_field: cp.ndarray, input_field: cp.ndarray):
+        _type_agnostic_copy_corners_y(output_field, input_field)
 
 
 @corner_copy_y.register(np.ndarray)
-def _corner_copy_y_nupy(field_to_copy: np.ndarray):
-    _blind_copy_corners_y(field_to_copy)
+def _corner_copy_y_numpy(output_field: np.ndarray, input_field: np.ndarray):
+    _type_agnostic_copy_corners_y(output_field, input_field)
 
 
 @corner_copy_y.register(Quantity)
-def _corner_copy_y_quantity(field_to_copy: Quantity):
-    _blind_copy_corners_y(field_to_copy.data)
+def _corner_copy_y_quantity(output_field: Quantity, input_field: Quantity):
+    _type_agnostic_copy_corners_y(output_field.data, input_field.data)
 
 
-def _blind_copy_corners_y(field_to_copy):
+def _type_agnostic_copy_corners_y(output_field, input_field):
     """Equivalent to the copy_corners_y functions in fortran.
 
     This is written to operate on plain ndarrarys and not use the GT4Py framework.
@@ -129,53 +129,53 @@ def _blind_copy_corners_y(field_to_copy):
         field_to_copy (ndarray): field to apply the corner copy on.
             This is explicitly not type-hinted for orchestration
     """
-    field_to_copy[0, 0] = field_to_copy[5, 0]
-    field_to_copy[1, 0] = field_to_copy[5, 1]
-    field_to_copy[2, 0] = field_to_copy[5, 2]
+    output_field[0, 0] = input_field[5, 0]
+    output_field[1, 0] = input_field[5, 1]
+    output_field[2, 0] = input_field[5, 2]
 
-    field_to_copy[0, 1] = field_to_copy[4, 0]
-    field_to_copy[1, 1] = field_to_copy[4, 1]
-    field_to_copy[2, 1] = field_to_copy[4, 2]
+    output_field[0, 1] = input_field[4, 0]
+    output_field[1, 1] = input_field[4, 1]
+    output_field[2, 1] = input_field[4, 2]
 
-    field_to_copy[0, 2] = field_to_copy[3, 0]
-    field_to_copy[1, 2] = field_to_copy[3, 1]
-    field_to_copy[2, 2] = field_to_copy[3, 2]
+    output_field[0, 2] = input_field[3, 0]
+    output_field[1, 2] = input_field[3, 1]
+    output_field[2, 2] = input_field[3, 2]
 
-    field_to_copy[-4, 0] = field_to_copy[-7, 2]
-    field_to_copy[-3, 0] = field_to_copy[-7, 1]
-    field_to_copy[-2, 0] = field_to_copy[-7, 0]
+    output_field[-4, 0] = input_field[-7, 2]
+    output_field[-3, 0] = input_field[-7, 1]
+    output_field[-2, 0] = input_field[-7, 0]
 
-    field_to_copy[-4, 1] = field_to_copy[-6, 2]
-    field_to_copy[-3, 1] = field_to_copy[-6, 1]
-    field_to_copy[-2, 1] = field_to_copy[-6, 0]
+    output_field[-4, 1] = input_field[-6, 2]
+    output_field[-3, 1] = input_field[-6, 1]
+    output_field[-2, 1] = input_field[-6, 0]
 
-    field_to_copy[-4, 2] = field_to_copy[-5, 2]
-    field_to_copy[-3, 2] = field_to_copy[-5, 1]
-    field_to_copy[-2, 2] = field_to_copy[-5, 0]
+    output_field[-4, 2] = input_field[-5, 2]
+    output_field[-3, 2] = input_field[-5, 1]
+    output_field[-2, 2] = input_field[-5, 0]
 
-    field_to_copy[0, -2] = field_to_copy[5, -2]
-    field_to_copy[0, -3] = field_to_copy[4, -2]
-    field_to_copy[0, -4] = field_to_copy[3, -2]
+    output_field[0, -2] = input_field[5, -2]
+    output_field[0, -3] = input_field[4, -2]
+    output_field[0, -4] = input_field[3, -2]
 
-    field_to_copy[1, -2] = field_to_copy[5, -3]
-    field_to_copy[1, -3] = field_to_copy[4, -3]
-    field_to_copy[1, -4] = field_to_copy[3, -3]
+    output_field[1, -2] = input_field[5, -3]
+    output_field[1, -3] = input_field[4, -3]
+    output_field[1, -4] = input_field[3, -3]
 
-    field_to_copy[2, -2] = field_to_copy[5, -4]
-    field_to_copy[2, -3] = field_to_copy[4, -4]
-    field_to_copy[2, -4] = field_to_copy[3, -4]
+    output_field[2, -2] = input_field[5, -4]
+    output_field[2, -3] = input_field[4, -4]
+    output_field[2, -4] = input_field[3, -4]
 
-    field_to_copy[-2, -4] = field_to_copy[-5, -2]
-    field_to_copy[-2, -3] = field_to_copy[-6, -2]
-    field_to_copy[-2, -2] = field_to_copy[-7, -2]
+    output_field[-2, -4] = input_field[-5, -2]
+    output_field[-2, -3] = input_field[-6, -2]
+    output_field[-2, -2] = input_field[-7, -2]
 
-    field_to_copy[-3, -4] = field_to_copy[-5, -3]
-    field_to_copy[-3, -3] = field_to_copy[-6, -3]
-    field_to_copy[-3, -2] = field_to_copy[-7, -3]
+    output_field[-3, -4] = input_field[-5, -3]
+    output_field[-3, -3] = input_field[-6, -3]
+    output_field[-3, -2] = input_field[-7, -3]
 
-    field_to_copy[-4, -4] = field_to_copy[-5, -4]
-    field_to_copy[-4, -3] = field_to_copy[-6, -4]
-    field_to_copy[-4, -2] = field_to_copy[-7, -4]
+    output_field[-4, -4] = input_field[-5, -4]
+    output_field[-4, -3] = input_field[-6, -4]
+    output_field[-4, -2] = input_field[-7, -4]
 
 
 class CopyCornersX(NDSLRuntime):
@@ -193,22 +193,22 @@ class CopyCornersX(NDSLRuntime):
 
         self._is_orch = stencil_factory.backend.is_orchestrated()
 
-    def _internal_corners_copy_3D(self, field: FloatFieldIJ):
-        _blind_copy_corners_x(field) if self._is_orch else corner_copy_x(field)
-
-    def _internal_corners_copy(self, field: FloatField, k: int):
+    def _internal_corners_copy_per_level(self, field: FloatField, k: int):
         if self._is_orch:
-            _blind_copy_corners_x(field[:, :, k])
+            _type_agnostic_copy_corners_x(field[:, :, k], field[:, :, k])
         else:
-            corner_copy_x(field)
+            corner_copy_x(field, field)
 
     def __call__(self, field: FloatField):
-        self._internal_corners_copy_3D(field)
+        if self._is_orch:
+            _type_agnostic_copy_corners_x(field, field)
+        else:
+            corner_copy_x(field, field)
 
     def nord(self, field: FloatField, nord: Quantity):
         for k in dace.map[0 : nord.shape[0]]:
             if nord[k] > 0:
-                self._internal_corners_copy(field, k)
+                self._internal_corners_copy_per_level(field, k)
 
 
 class CopyCornersY(NDSLRuntime):
@@ -227,19 +227,19 @@ class CopyCornersY(NDSLRuntime):
 
         self._is_orch = stencil_factory.backend.is_orchestrated()
 
-    def _internal_corners_copy_3D(self, field: FloatFieldIJ):
-        _blind_copy_corners_y(field) if self._is_orch else corner_copy_y(field)
-
-    def _internal_corners_copy(self, field: FloatField, k: int):
+    def _internal_corners_copy_per_level(self, field: FloatField, k: int):
         if self._is_orch:
-            _blind_copy_corners_y(field[:, :, k])
+            _type_agnostic_copy_corners_y(field[:, :, k], field[:, :, k])
         else:
-            corner_copy_y(field[:, :, k])
+            corner_copy_y(field[:, :, k], field[:, :, k])
 
     def __call__(self, field: FloatField):
-        self._internal_corners_copy_3D(field)
+        if self._is_orch:
+            _type_agnostic_copy_corners_y(field, field)
+        else:
+            corner_copy_y(field, field)
 
     def nord(self, field: FloatField, nord: Quantity):
         for k in dace.map[0 : nord.shape[0]]:
             if nord[k] > 0:
-                self._internal_corners_copy(field, k)
+                self._internal_corners_copy_per_level(field, k)
