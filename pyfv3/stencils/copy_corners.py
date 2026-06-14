69742cac97e27ce70b0bc37c5413fd1975142981
Florian Deconinck <deconinck.florian@gmail.com>
2026-06-14 17:24:39 -0400

    Fix `copy_corner`: take topology into account

--- pyfv3/stencils/copy_corners.py
+++ pyfv3/stencils/copy_corners.py
@@ -16,6 +16,10 @@ class CopyCornersX(NDSLRuntime):
             config=stencil_factory.config.dace_config,
             method_to_orchestrate="nord",
         )
+        self._is_ne_corner = stencil_factory.grid_indexing.ne_corner
+        self._is_nw_corner = stencil_factory.grid_indexing.nw_corner
+        self._is_sw_corner = stencil_factory.grid_indexing.sw_corner
+        self._is_se_corner = stencil_factory.grid_indexing.se_corner
 
         if stencil_factory.grid_indexing.n_halo != 3:
             raise NotImplementedError(
@@ -24,57 +28,7 @@ class CopyCornersX(NDSLRuntime):
 
     def __call__(self, field: FloatField):
         for k in dace.map[0 : field.shape[2]]:
-            field[0, 0, k] = field[0, 5, k]
-            field[0, 1, k] = field[1, 5, k]
-            field[0, 2, k] = field[2, 5, k]
-
-            field[1, 0, k] = field[0, 4, k]
-            field[1, 1, k] = field[1, 4, k]
-            field[1, 2, k] = field[2, 4, k]
-
-            field[2, 0, k] = field[0, 3, k]
-            field[2, 1, k] = field[1, 3, k]
-            field[2, 2, k] = field[2, 3, k]
-
-            field[0, -4, k] = field[2, -7, k]
-            field[0, -3, k] = field[1, -7, k]
-            field[0, -2, k] = field[0, -7, k]
-
-            field[1, -4, k] = field[2, -6, k]
-            field[1, -3, k] = field[1, -6, k]
-            field[1, -2, k] = field[0, -6, k]
-
-            field[2, -4, k] = field[2, -5, k]
-            field[2, -3, k] = field[1, -5, k]
-            field[2, -2, k] = field[0, -5, k]
-
-            field[-4, 0, k] = field[-2, 3, k]
-            field[-4, 1, k] = field[-3, 3, k]
-            field[-4, 2, k] = field[-4, 3, k]
-
-            field[-3, 0, k] = field[-2, 4, k]
-            field[-3, 1, k] = field[-3, 4, k]
-            field[-3, 2, k] = field[-4, 4, k]
-
-            field[-2, 0, k] = field[-2, 5, k]
-            field[-2, 1, k] = field[-3, 5, k]
-            field[-2, 2, k] = field[-4, 5, k]
-
-            field[-4, -2, k] = field[-2, -5, k]
-            field[-4, -3, k] = field[-3, -5, k]
-            field[-4, -4, k] = field[-4, -5, k]
-
-            field[-3, -2, k] = field[-2, -6, k]
-            field[-3, -3, k] = field[-3, -6, k]
-            field[-3, -4, k] = field[-4, -6, k]
-
-            field[-2, -2, k] = field[-2, -7, k]
-            field[-2, -3, k] = field[-3, -7, k]
-            field[-2, -4, k] = field[-4, -7, k]
-
-    def nord(self, field: FloatField, nord: Quantity):
-        for k in dace.map[0 : nord.shape[0]]:
-            if nord[k] > 0:
+            if self._is_sw_corner:
                 field[0, 0, k] = field[0, 5, k]
                 field[0, 1, k] = field[1, 5, k]
                 field[0, 2, k] = field[2, 5, k]
@@ -87,6 +41,7 @@ class CopyCornersX(NDSLRuntime):
                 field[2, 1, k] = field[1, 3, k]
                 field[2, 2, k] = field[2, 3, k]
 
+            if self._is_se_corner:
                 field[0, -4, k] = field[2, -7, k]
                 field[0, -3, k] = field[1, -7, k]
                 field[0, -2, k] = field[0, -7, k]
@@ -99,6 +54,7 @@ class CopyCornersX(NDSLRuntime):
                 field[2, -3, k] = field[1, -5, k]
                 field[2, -2, k] = field[0, -5, k]
 
+            if self._is_nw_corner:
                 field[-4, 0, k] = field[-2, 3, k]
                 field[-4, 1, k] = field[-3, 3, k]
                 field[-4, 2, k] = field[-4, 3, k]
@@ -111,6 +67,7 @@ class CopyCornersX(NDSLRuntime):
                 field[-2, 1, k] = field[-3, 5, k]
                 field[-2, 2, k] = field[-4, 5, k]
 
+            if self._is_ne_corner:
                 field[-4, -2, k] = field[-2, -5, k]
                 field[-4, -3, k] = field[-3, -5, k]
                 field[-4, -4, k] = field[-4, -5, k]
@@ -122,6 +79,61 @@ class CopyCornersX(NDSLRuntime):
                 field[-2, -2, k] = field[-2, -7, k]
                 field[-2, -3, k] = field[-3, -7, k]
                 field[-2, -4, k] = field[-4, -7, k]
+
+    def nord(self, field: FloatField, nord: Quantity):
+        for k in dace.map[0 : nord.shape[0]]:
+            if nord[k] > 0:
+                if self._is_sw_corner:
+                    field[0, 0, k] = field[0, 5, k]
+                    field[0, 1, k] = field[1, 5, k]
+                    field[0, 2, k] = field[2, 5, k]
+
+                    field[1, 0, k] = field[0, 4, k]
+                    field[1, 1, k] = field[1, 4, k]
+                    field[1, 2, k] = field[2, 4, k]
+
+                    field[2, 0, k] = field[0, 3, k]
+                    field[2, 1, k] = field[1, 3, k]
+                    field[2, 2, k] = field[2, 3, k]
+
+                if self._is_se_corner:
+                    field[0, -4, k] = field[2, -7, k]
+                    field[0, -3, k] = field[1, -7, k]
+                    field[0, -2, k] = field[0, -7, k]
+
+                    field[1, -4, k] = field[2, -6, k]
+                    field[1, -3, k] = field[1, -6, k]
+                    field[1, -2, k] = field[0, -6, k]
+
+                    field[2, -4, k] = field[2, -5, k]
+                    field[2, -3, k] = field[1, -5, k]
+                    field[2, -2, k] = field[0, -5, k]
+
+                if self._is_nw_corner:
+                    field[-4, 0, k] = field[-2, 3, k]
+                    field[-4, 1, k] = field[-3, 3, k]
+                    field[-4, 2, k] = field[-4, 3, k]
+
+                    field[-3, 0, k] = field[-2, 4, k]
+                    field[-3, 1, k] = field[-3, 4, k]
+                    field[-3, 2, k] = field[-4, 4, k]
+
+                    field[-2, 0, k] = field[-2, 5, k]
+                    field[-2, 1, k] = field[-3, 5, k]
+                    field[-2, 2, k] = field[-4, 5, k]
+
+                if self._is_ne_corner:
+                    field[-4, -2, k] = field[-2, -5, k]
+                    field[-4, -3, k] = field[-3, -5, k]
+                    field[-4, -4, k] = field[-4, -5, k]
+
+                    field[-3, -2, k] = field[-2, -6, k]
+                    field[-3, -3, k] = field[-3, -6, k]
+                    field[-3, -4, k] = field[-4, -6, k]
+
+                    field[-2, -2, k] = field[-2, -7, k]
+                    field[-2, -3, k] = field[-3, -7, k]
+                    field[-2, -4, k] = field[-4, -7, k]
 
 
 class CopyCornersY(NDSLRuntime):
@@ -137,6 +149,11 @@ class CopyCornersY(NDSLRuntime):
             method_to_orchestrate="nord",
         )
 
+        self._is_ne_corner = stencil_factory.grid_indexing.ne_corner
+        self._is_nw_corner = stencil_factory.grid_indexing.nw_corner
+        self._is_sw_corner = stencil_factory.grid_indexing.sw_corner
+        self._is_se_corner = stencil_factory.grid_indexing.se_corner
+
         if stencil_factory.grid_indexing.n_halo != 3:
             raise NotImplementedError(
                 "CopyCornersY is only implemented for exactly 3 halo points."
@@ -144,57 +161,7 @@ class CopyCornersY(NDSLRuntime):
 
     def __call__(self, field: FloatField):
         for k in dace.map[0 : field.shape[2]]:
-            field[0, 0, k] = field[5, 0, k]
-            field[1, 0, k] = field[5, 1, k]
-            field[2, 0, k] = field[5, 2, k]
-
-            field[0, 1, k] = field[4, 0, k]
-            field[1, 1, k] = field[4, 1, k]
-            field[2, 1, k] = field[4, 2, k]
-
-            field[0, 2, k] = field[3, 0, k]
-            field[1, 2, k] = field[3, 1, k]
-            field[2, 2, k] = field[3, 2, k]
-
-            field[-4, 0, k] = field[-7, 2, k]
-            field[-3, 0, k] = field[-7, 1, k]
-            field[-2, 0, k] = field[-7, 0, k]
-
-            field[-4, 1, k] = field[-6, 2, k]
-            field[-3, 1, k] = field[-6, 1, k]
-            field[-2, 1, k] = field[-6, 0, k]
-
-            field[-4, 2, k] = field[-5, 2, k]
-            field[-3, 2, k] = field[-5, 1, k]
-            field[-2, 2, k] = field[-5, 0, k]
-
-            field[0, -2, k] = field[5, -2, k]
-            field[0, -3, k] = field[4, -2, k]
-            field[0, -4, k] = field[3, -2, k]
-
-            field[1, -2, k] = field[5, -3, k]
-            field[1, -3, k] = field[4, -3, k]
-            field[1, -4, k] = field[3, -3, k]
-
-            field[2, -2, k] = field[5, -4, k]
-            field[2, -3, k] = field[4, -4, k]
-            field[2, -4, k] = field[3, -4, k]
-
-            field[-2, -4, k] = field[-5, -2, k]
-            field[-2, -3, k] = field[-6, -2, k]
-            field[-2, -2, k] = field[-7, -2, k]
-
-            field[-3, -4, k] = field[-5, -3, k]
-            field[-3, -3, k] = field[-6, -3, k]
-            field[-3, -2, k] = field[-7, -3, k]
-
-            field[-4, -4, k] = field[-5, -4, k]
-            field[-4, -3, k] = field[-6, -4, k]
-            field[-4, -2, k] = field[-7, -4, k]
-
-    def nord(self, field: FloatField, nord: Quantity):
-        for k in dace.map[0 : nord.shape[0]]:
-            if nord[k] > 0:
+            if self._is_sw_corner:
                 field[0, 0, k] = field[5, 0, k]
                 field[1, 0, k] = field[5, 1, k]
                 field[2, 0, k] = field[5, 2, k]
@@ -207,6 +174,7 @@ class CopyCornersY(NDSLRuntime):
                 field[1, 2, k] = field[3, 1, k]
                 field[2, 2, k] = field[3, 2, k]
 
+            if self._is_se_corner:
                 field[-4, 0, k] = field[-7, 2, k]
                 field[-3, 0, k] = field[-7, 1, k]
                 field[-2, 0, k] = field[-7, 0, k]
@@ -219,6 +187,7 @@ class CopyCornersY(NDSLRuntime):
                 field[-3, 2, k] = field[-5, 1, k]
                 field[-2, 2, k] = field[-5, 0, k]
 
+            if self._is_nw_corner:
                 field[0, -2, k] = field[5, -2, k]
                 field[0, -3, k] = field[4, -2, k]
                 field[0, -4, k] = field[3, -2, k]
@@ -231,6 +200,7 @@ class CopyCornersY(NDSLRuntime):
                 field[2, -3, k] = field[4, -4, k]
                 field[2, -4, k] = field[3, -4, k]
 
+            if self._is_ne_corner:
                 field[-2, -4, k] = field[-5, -2, k]
                 field[-2, -3, k] = field[-6, -2, k]
                 field[-2, -2, k] = field[-7, -2, k]
@@ -242,3 +212,58 @@ class CopyCornersY(NDSLRuntime):
                 field[-4, -4, k] = field[-5, -4, k]
                 field[-4, -3, k] = field[-6, -4, k]
                 field[-4, -2, k] = field[-7, -4, k]
+
+    def nord(self, field: FloatField, nord: Quantity):
+        for k in dace.map[0 : nord.shape[0]]:
+            if nord[k] > 0:
+                if self._is_sw_corner:
+                    field[0, 0, k] = field[5, 0, k]
+                    field[1, 0, k] = field[5, 1, k]
+                    field[2, 0, k] = field[5, 2, k]
+
+                    field[0, 1, k] = field[4, 0, k]
+                    field[1, 1, k] = field[4, 1, k]
+                    field[2, 1, k] = field[4, 2, k]
+
+                    field[0, 2, k] = field[3, 0, k]
+                    field[1, 2, k] = field[3, 1, k]
+                    field[2, 2, k] = field[3, 2, k]
+
+                if self._is_se_corner:
+                    field[-4, 0, k] = field[-7, 2, k]
+                    field[-3, 0, k] = field[-7, 1, k]
+                    field[-2, 0, k] = field[-7, 0, k]
+
+                    field[-4, 1, k] = field[-6, 2, k]
+                    field[-3, 1, k] = field[-6, 1, k]
+                    field[-2, 1, k] = field[-6, 0, k]
+
+                    field[-4, 2, k] = field[-5, 2, k]
+                    field[-3, 2, k] = field[-5, 1, k]
+                    field[-2, 2, k] = field[-5, 0, k]
+
+                if self._is_nw_corner:
+                    field[0, -2, k] = field[5, -2, k]
+                    field[0, -3, k] = field[4, -2, k]
+                    field[0, -4, k] = field[3, -2, k]
+
+                    field[1, -2, k] = field[5, -3, k]
+                    field[1, -3, k] = field[4, -3, k]
+                    field[1, -4, k] = field[3, -3, k]
+
+                    field[2, -2, k] = field[5, -4, k]
+                    field[2, -3, k] = field[4, -4, k]
+                    field[2, -4, k] = field[3, -4, k]
+
+                if self._is_ne_corner:
+                    field[-2, -4, k] = field[-5, -2, k]
+                    field[-2, -3, k] = field[-6, -2, k]
+                    field[-2, -2, k] = field[-7, -2, k]
+
+                    field[-3, -4, k] = field[-5, -3, k]
+                    field[-3, -3, k] = field[-6, -3, k]
+                    field[-3, -2, k] = field[-7, -3, k]
+
+                    field[-4, -4, k] = field[-5, -4, k]
+                    field[-4, -3, k] = field[-6, -4, k]
+                    field[-4, -2, k] = field[-7, -4, k]
