9483132ccca481dce1d09128dda127ddb1c70210
Florian Deconinck <deconinck.florian@gmail.com>
2026-06-21 14:56:25 -0400

    Kernalize in GPU

--- pyfv3/optimization.py
+++ pyfv3/optimization.py
@@ -5,9 +5,9 @@ def get_optimization_config(backend: Backend) -> OptimizationConfig:
     if backend.is_gpu_backend():
         return OptimizationConfig(
             stree=OptimizationConfig.Tree(
-                enabled=False,
+                enabled=True,
                 kernalize=True,
-                merger=OptimizationConfig.Tree.Merger(enabled=True, overcompute=False),
+                merger=OptimizationConfig.Tree.Merger(enabled=False, overcompute=False),
             ),
             gpu=OptimizationConfig.GPU(common_gpu_xforms=False),
         )
