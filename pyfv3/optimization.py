823d5ecedeffd273a5c927fd0ac989d7707ac2d1
Florian Deconinck <deconinck.florian@gmail.com>
2026-06-21 12:40:31 -0400

    Deactivate all schedule tree on FV

--- pyfv3/optimization.py
+++ pyfv3/optimization.py
@@ -5,16 +5,16 @@ def get_optimization_config(backend: Backend) -> OptimizationConfig:
     if backend.is_gpu_backend():
         return OptimizationConfig(
             stree=OptimizationConfig.Tree(
-                enabled=True,
+                enabled=False,
                 kernalize=True,
-                merger=OptimizationConfig.Tree.Merger(enabled=False, overcompute=False),
+                merger=OptimizationConfig.Tree.Merger(enabled=True, overcompute=False),
             ),
             gpu=OptimizationConfig.GPU(common_gpu_xforms=False),
         )
     else:
         return OptimizationConfig(
             stree=OptimizationConfig.Tree(
-                enabled=True,
+                enabled=False,
                 kernalize=False,
                 merger=OptimizationConfig.Tree.Merger(enabled=True, overcompute=False),
             ),
