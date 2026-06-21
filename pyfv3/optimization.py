2b70afb2e3c799aa3daab0be49e8bb1e65608586
Florian Deconinck <deconinck.florian@gmail.com>
2026-06-21 09:38:39 -0400

    Centralize OptimizationConfig

---
+++ pyfv3/optimization.py
@@ -0,0 +1,26 @@
+from ndsl import Backend, OptimizationConfig
+
+def get_optimization_config(backend: Backend):
+    if backend.is_gpu_backend():
+        return OptimizationConfig(
+            stree=OptimizationConfig.Tree(
+                enabled=True,
+                kernalize=True,
+                merger=OptimizationConfig.Tree.Merger(
+                    enabled=False,
+                    overcompute=False
+                )
+            ),
+            gpu=OptimizationConfig.GPU(common_gpu_xforms=False)
+        )
+    else:
+        return OptimizationConfig(
+            stree=OptimizationConfig.Tree(
+                enabled=True,
+                kernalize=False,
+                merger=OptimizationConfig.Tree.Merger(
+                    enabled=True,
+                    overcompute=False
+                )
+            ),
+        )
