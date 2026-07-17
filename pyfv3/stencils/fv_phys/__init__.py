711184b6da090d358f0c226af6153f5119f614d0
Oliver Elbert <oliver.elbert36@gmail.com>
2026-07-17 15:12:33 -0400

    cleanup init

--- pyfv3/stencils/fv_phys/__init__.py
+++ pyfv3/stencils/fv_phys/__init__.py
@@ -1,2 +1,13 @@
 from _config import GrayRadiationConfig
 from gray_radiation_standalone import GrayRadSolo
+
+
+"""
+GrayRadiationConfig: Configuration class for the gray radiation scheme
+GrayRadSolo: Gray radiation class
+"""
+
+__all__ = [
+    "GrayRadiationConfig",
+    "GrayRadSolo",
+]
