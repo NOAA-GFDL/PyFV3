587d603491cc9468caba3a2ca9866936437207dc
Florian Deconinck <deconinck.florian@gmail.com>
2026-08-03 09:05:43 -0400

    Use K axis name that helps the schedule tree

--- pyfv3/stencils/copy_corners.py
+++ pyfv3/stencils/copy_corners.py
@@ -27,113 +27,113 @@ class CopyCornersX(NDSLRuntime):
             )
 
     def __call__(self, field: FloatField):
-        for k in dace.map[0 : field.shape[2]]:
+        for __k in dace.map[0 : field.shape[2]]:
             if self._is_sw_corner:
-                field[0, 0, k] = field[0, 5, k]
-                field[0, 1, k] = field[1, 5, k]
-                field[0, 2, k] = field[2, 5, k]
+                field[0, 0, __k] = field[0, 5, __k]
+                field[0, 1, __k] = field[1, 5, __k]
+                field[0, 2, __k] = field[2, 5, __k]
 
-                field[1, 0, k] = field[0, 4, k]
-                field[1, 1, k] = field[1, 4, k]
-                field[1, 2, k] = field[2, 4, k]
+                field[1, 0, __k] = field[0, 4, __k]
+                field[1, 1, __k] = field[1, 4, __k]
+                field[1, 2, __k] = field[2, 4, __k]
 
-                field[2, 0, k] = field[0, 3, k]
-                field[2, 1, k] = field[1, 3, k]
-                field[2, 2, k] = field[2, 3, k]
+                field[2, 0, __k] = field[0, 3, __k]
+                field[2, 1, __k] = field[1, 3, __k]
+                field[2, 2, __k] = field[2, 3, __k]
 
             if self._is_se_corner:
-                field[-4, 0, k] = field[-2, 3, k]
-                field[-4, 1, k] = field[-3, 3, k]
-                field[-4, 2, k] = field[-4, 3, k]
+                field[-4, 0, __k] = field[-2, 3, __k]
+                field[-4, 1, __k] = field[-3, 3, __k]
+                field[-4, 2, __k] = field[-4, 3, __k]
 
-                field[-3, 0, k] = field[-2, 4, k]
-                field[-3, 1, k] = field[-3, 4, k]
-                field[-3, 2, k] = field[-4, 4, k]
+                field[-3, 0, __k] = field[-2, 4, __k]
+                field[-3, 1, __k] = field[-3, 4, __k]
+                field[-3, 2, __k] = field[-4, 4, __k]
 
-                field[-2, 0, k] = field[-2, 5, k]
-                field[-2, 1, k] = field[-3, 5, k]
-                field[-2, 2, k] = field[-4, 5, k]
+                field[-2, 0, __k] = field[-2, 5, __k]
+                field[-2, 1, __k] = field[-3, 5, __k]
+                field[-2, 2, __k] = field[-4, 5, __k]
 
             if self._is_nw_corner:
-                field[0, -4, k] = field[2, -7, k]
-                field[0, -3, k] = field[1, -7, k]
-                field[0, -2, k] = field[0, -7, k]
+                field[0, -4, __k] = field[2, -7, __k]
+                field[0, -3, __k] = field[1, -7, __k]
+                field[0, -2, __k] = field[0, -7, __k]
 
-                field[1, -4, k] = field[2, -6, k]
-                field[1, -3, k] = field[1, -6, k]
-                field[1, -2, k] = field[0, -6, k]
+                field[1, -4, __k] = field[2, -6, __k]
+                field[1, -3, __k] = field[1, -6, __k]
+                field[1, -2, __k] = field[0, -6, __k]
 
-                field[2, -4, k] = field[2, -5, k]
-                field[2, -3, k] = field[1, -5, k]
-                field[2, -2, k] = field[0, -5, k]
+                field[2, -4, __k] = field[2, -5, __k]
+                field[2, -3, __k] = field[1, -5, __k]
+                field[2, -2, __k] = field[0, -5, __k]
 
             if self._is_ne_corner:
-                field[-4, -2, k] = field[-2, -5, k]
-                field[-4, -3, k] = field[-3, -5, k]
-                field[-4, -4, k] = field[-4, -5, k]
+                field[-4, -2, __k] = field[-2, -5, __k]
+                field[-4, -3, __k] = field[-3, -5, __k]
+                field[-4, -4, __k] = field[-4, -5, __k]
 
-                field[-3, -2, k] = field[-2, -6, k]
-                field[-3, -3, k] = field[-3, -6, k]
-                field[-3, -4, k] = field[-4, -6, k]
+                field[-3, -2, __k] = field[-2, -6, __k]
+                field[-3, -3, __k] = field[-3, -6, __k]
+                field[-3, -4, __k] = field[-4, -6, __k]
 
-                field[-2, -2, k] = field[-2, -7, k]
-                field[-2, -3, k] = field[-3, -7, k]
-                field[-2, -4, k] = field[-4, -7, k]
+                field[-2, -2, __k] = field[-2, -7, __k]
+                field[-2, -3, __k] = field[-3, -7, __k]
+                field[-2, -4, __k] = field[-4, -7, __k]
 
     def nord(self, field: FloatField, nord: Quantity):
-        for k in dace.map[0 : nord.shape[0]]:
-            if nord[k] > 0:
+        for __k in dace.map[0 : nord.shape[0]]:
+            if nord[__k] > 0:
                 if self._is_sw_corner:
-                    field[0, 0, k] = field[0, 5, k]
-                    field[0, 1, k] = field[1, 5, k]
-                    field[0, 2, k] = field[2, 5, k]
+                    field[0, 0, __k] = field[0, 5, __k]
+                    field[0, 1, __k] = field[1, 5, __k]
+                    field[0, 2, __k] = field[2, 5, __k]
 
-                    field[1, 0, k] = field[0, 4, k]
-                    field[1, 1, k] = field[1, 4, k]
-                    field[1, 2, k] = field[2, 4, k]
+                    field[1, 0, __k] = field[0, 4, __k]
+                    field[1, 1, __k] = field[1, 4, __k]
+                    field[1, 2, __k] = field[2, 4, __k]
 
-                    field[2, 0, k] = field[0, 3, k]
-                    field[2, 1, k] = field[1, 3, k]
-                    field[2, 2, k] = field[2, 3, k]
+                    field[2, 0, __k] = field[0, 3, __k]
+                    field[2, 1, __k] = field[1, 3, __k]
+                    field[2, 2, __k] = field[2, 3, __k]
 
                 if self._is_se_corner:
-                    field[-4, 0, k] = field[-2, 3, k]
-                    field[-4, 1, k] = field[-3, 3, k]
-                    field[-4, 2, k] = field[-4, 3, k]
+                    field[-4, 0, __k] = field[-2, 3, __k]
+                    field[-4, 1, __k] = field[-3, 3, __k]
+                    field[-4, 2, __k] = field[-4, 3, __k]
 
-                    field[-3, 0, k] = field[-2, 4, k]
-                    field[-3, 1, k] = field[-3, 4, k]
-                    field[-3, 2, k] = field[-4, 4, k]
+                    field[-3, 0, __k] = field[-2, 4, __k]
+                    field[-3, 1, __k] = field[-3, 4, __k]
+                    field[-3, 2, __k] = field[-4, 4, __k]
 
-                    field[-2, 0, k] = field[-2, 5, k]
-                    field[-2, 1, k] = field[-3, 5, k]
-                    field[-2, 2, k] = field[-4, 5, k]
+                    field[-2, 0, __k] = field[-2, 5, __k]
+                    field[-2, 1, __k] = field[-3, 5, __k]
+                    field[-2, 2, __k] = field[-4, 5, __k]
 
                 if self._is_nw_corner:
-                    field[0, -4, k] = field[2, -7, k]
-                    field[0, -3, k] = field[1, -7, k]
-                    field[0, -2, k] = field[0, -7, k]
+                    field[0, -4, __k] = field[2, -7, __k]
+                    field[0, -3, __k] = field[1, -7, __k]
+                    field[0, -2, __k] = field[0, -7, __k]
 
-                    field[1, -4, k] = field[2, -6, k]
-                    field[1, -3, k] = field[1, -6, k]
-                    field[1, -2, k] = field[0, -6, k]
+                    field[1, -4, __k] = field[2, -6, __k]
+                    field[1, -3, __k] = field[1, -6, __k]
+                    field[1, -2, __k] = field[0, -6, __k]
 
-                    field[2, -4, k] = field[2, -5, k]
-                    field[2, -3, k] = field[1, -5, k]
-                    field[2, -2, k] = field[0, -5, k]
+                    field[2, -4, __k] = field[2, -5, __k]
+                    field[2, -3, __k] = field[1, -5, __k]
+                    field[2, -2, __k] = field[0, -5, __k]
 
                 if self._is_ne_corner:
-                    field[-4, -2, k] = field[-2, -5, k]
-                    field[-4, -3, k] = field[-3, -5, k]
-                    field[-4, -4, k] = field[-4, -5, k]
+                    field[-4, -2, __k] = field[-2, -5, __k]
+                    field[-4, -3, __k] = field[-3, -5, __k]
+                    field[-4, -4, __k] = field[-4, -5, __k]
 
-                    field[-3, -2, k] = field[-2, -6, k]
-                    field[-3, -3, k] = field[-3, -6, k]
-                    field[-3, -4, k] = field[-4, -6, k]
+                    field[-3, -2, __k] = field[-2, -6, __k]
+                    field[-3, -3, __k] = field[-3, -6, __k]
+                    field[-3, -4, __k] = field[-4, -6, __k]
 
-                    field[-2, -2, k] = field[-2, -7, k]
-                    field[-2, -3, k] = field[-3, -7, k]
-                    field[-2, -4, k] = field[-4, -7, k]
+                    field[-2, -2, __k] = field[-2, -7, __k]
+                    field[-2, -3, __k] = field[-3, -7, __k]
+                    field[-2, -4, __k] = field[-4, -7, __k]
 
 
 class CopyCornersY(NDSLRuntime):
@@ -160,110 +160,110 @@ class CopyCornersY(NDSLRuntime):
             )
 
     def __call__(self, field: FloatField):
-        for k in dace.map[0 : field.shape[2]]:
+        for __k in dace.map[0 : field.shape[2]]:
             if self._is_sw_corner:
-                field[0, 0, k] = field[5, 0, k]
-                field[1, 0, k] = field[5, 1, k]
-                field[2, 0, k] = field[5, 2, k]
+                field[0, 0, __k] = field[5, 0, __k]
+                field[1, 0, __k] = field[5, 1, __k]
+                field[2, 0, __k] = field[5, 2, __k]
 
-                field[0, 1, k] = field[4, 0, k]
-                field[1, 1, k] = field[4, 1, k]
-                field[2, 1, k] = field[4, 2, k]
+                field[0, 1, __k] = field[4, 0, __k]
+                field[1, 1, __k] = field[4, 1, __k]
+                field[2, 1, __k] = field[4, 2, __k]
 
-                field[0, 2, k] = field[3, 0, k]
-                field[1, 2, k] = field[3, 1, k]
-                field[2, 2, k] = field[3, 2, k]
+                field[0, 2, __k] = field[3, 0, __k]
+                field[1, 2, __k] = field[3, 1, __k]
+                field[2, 2, __k] = field[3, 2, __k]
 
             if self._is_se_corner:
-                field[-4, 0, k] = field[-7, 2, k]
-                field[-3, 0, k] = field[-7, 1, k]
-                field[-2, 0, k] = field[-7, 0, k]
+                field[-4, 0, __k] = field[-7, 2, __k]
+                field[-3, 0, __k] = field[-7, 1, __k]
+                field[-2, 0, __k] = field[-7, 0, __k]
 
-                field[-4, 1, k] = field[-6, 2, k]
-                field[-3, 1, k] = field[-6, 1, k]
-                field[-2, 1, k] = field[-6, 0, k]
+                field[-4, 1, __k] = field[-6, 2, __k]
+                field[-3, 1, __k] = field[-6, 1, __k]
+                field[-2, 1, __k] = field[-6, 0, __k]
 
-                field[-4, 2, k] = field[-5, 2, k]
-                field[-3, 2, k] = field[-5, 1, k]
-                field[-2, 2, k] = field[-5, 0, k]
+                field[-4, 2, __k] = field[-5, 2, __k]
+                field[-3, 2, __k] = field[-5, 1, __k]
+                field[-2, 2, __k] = field[-5, 0, __k]
 
             if self._is_nw_corner:
-                field[0, -2, k] = field[5, -2, k]
-                field[0, -3, k] = field[4, -2, k]
-                field[0, -4, k] = field[3, -2, k]
+                field[0, -2, __k] = field[5, -2, __k]
+                field[0, -3, __k] = field[4, -2, __k]
+                field[0, -4, __k] = field[3, -2, __k]
 
-                field[1, -2, k] = field[5, -3, k]
-                field[1, -3, k] = field[4, -3, k]
-                field[1, -4, k] = field[3, -3, k]
+                field[1, -2, __k] = field[5, -3, __k]
+                field[1, -3, __k] = field[4, -3, __k]
+                field[1, -4, __k] = field[3, -3, __k]
 
-                field[2, -2, k] = field[5, -4, k]
-                field[2, -3, k] = field[4, -4, k]
-                field[2, -4, k] = field[3, -4, k]
+                field[2, -2, __k] = field[5, -4, __k]
+                field[2, -3, __k] = field[4, -4, __k]
+                field[2, -4, __k] = field[3, -4, __k]
 
             if self._is_ne_corner:
-                field[-2, -4, k] = field[-5, -2, k]
-                field[-2, -3, k] = field[-6, -2, k]
-                field[-2, -2, k] = field[-7, -2, k]
+                field[-2, -4, __k] = field[-5, -2, __k]
+                field[-2, -3, __k] = field[-6, -2, __k]
+                field[-2, -2, __k] = field[-7, -2, __k]
 
-                field[-3, -4, k] = field[-5, -3, k]
-                field[-3, -3, k] = field[-6, -3, k]
-                field[-3, -2, k] = field[-7, -3, k]
+                field[-3, -4, __k] = field[-5, -3, __k]
+                field[-3, -3, __k] = field[-6, -3, __k]
+                field[-3, -2, __k] = field[-7, -3, __k]
 
-                field[-4, -4, k] = field[-5, -4, k]
-                field[-4, -3, k] = field[-6, -4, k]
-                field[-4, -2, k] = field[-7, -4, k]
+                field[-4, -4, __k] = field[-5, -4, __k]
+                field[-4, -3, __k] = field[-6, -4, __k]
+                field[-4, -2, __k] = field[-7, -4, __k]
 
     def nord(self, field: FloatField, nord: Quantity):
-        for k in dace.map[0 : nord.shape[0]]:
-            if nord[k] > 0:
+        for __k in dace.map[0 : nord.shape[0]]:
+            if nord[__k] > 0:
                 if self._is_sw_corner:
-                    field[0, 0, k] = field[5, 0, k]
-                    field[1, 0, k] = field[5, 1, k]
-                    field[2, 0, k] = field[5, 2, k]
+                    field[0, 0, __k] = field[5, 0, __k]
+                    field[1, 0, __k] = field[5, 1, __k]
+                    field[2, 0, __k] = field[5, 2, __k]
 
-                    field[0, 1, k] = field[4, 0, k]
-                    field[1, 1, k] = field[4, 1, k]
-                    field[2, 1, k] = field[4, 2, k]
+                    field[0, 1, __k] = field[4, 0, __k]
+                    field[1, 1, __k] = field[4, 1, __k]
+                    field[2, 1, __k] = field[4, 2, __k]
 
-                    field[0, 2, k] = field[3, 0, k]
-                    field[1, 2, k] = field[3, 1, k]
-                    field[2, 2, k] = field[3, 2, k]
+                    field[0, 2, __k] = field[3, 0, __k]
+                    field[1, 2, __k] = field[3, 1, __k]
+                    field[2, 2, __k] = field[3, 2, __k]
 
                 if self._is_se_corner:
-                    field[-4, 0, k] = field[-7, 2, k]
-                    field[-3, 0, k] = field[-7, 1, k]
-                    field[-2, 0, k] = field[-7, 0, k]
+                    field[-4, 0, __k] = field[-7, 2, __k]
+                    field[-3, 0, __k] = field[-7, 1, __k]
+                    field[-2, 0, __k] = field[-7, 0, __k]
 
-                    field[-4, 1, k] = field[-6, 2, k]
-                    field[-3, 1, k] = field[-6, 1, k]
-                    field[-2, 1, k] = field[-6, 0, k]
+                    field[-4, 1, __k] = field[-6, 2, __k]
+                    field[-3, 1, __k] = field[-6, 1, __k]
+                    field[-2, 1, __k] = field[-6, 0, __k]
 
-                    field[-4, 2, k] = field[-5, 2, k]
-                    field[-3, 2, k] = field[-5, 1, k]
-                    field[-2, 2, k] = field[-5, 0, k]
+                    field[-4, 2, __k] = field[-5, 2, __k]
+                    field[-3, 2, __k] = field[-5, 1, __k]
+                    field[-2, 2, __k] = field[-5, 0, __k]
 
                 if self._is_nw_corner:
-                    field[0, -2, k] = field[5, -2, k]
-                    field[0, -3, k] = field[4, -2, k]
-                    field[0, -4, k] = field[3, -2, k]
+                    field[0, -2, __k] = field[5, -2, __k]
+                    field[0, -3, __k] = field[4, -2, __k]
+                    field[0, -4, __k] = field[3, -2, __k]
 
-                    field[1, -2, k] = field[5, -3, k]
-                    field[1, -3, k] = field[4, -3, k]
-                    field[1, -4, k] = field[3, -3, k]
+                    field[1, -2, __k] = field[5, -3, __k]
+                    field[1, -3, __k] = field[4, -3, __k]
+                    field[1, -4, __k] = field[3, -3, __k]
 
-                    field[2, -2, k] = field[5, -4, k]
-                    field[2, -3, k] = field[4, -4, k]
-                    field[2, -4, k] = field[3, -4, k]
+                    field[2, -2, __k] = field[5, -4, __k]
+                    field[2, -3, __k] = field[4, -4, __k]
+                    field[2, -4, __k] = field[3, -4, __k]
 
                 if self._is_ne_corner:
-                    field[-2, -4, k] = field[-5, -2, k]
-                    field[-2, -3, k] = field[-6, -2, k]
-                    field[-2, -2, k] = field[-7, -2, k]
+                    field[-2, -4, __k] = field[-5, -2, __k]
+                    field[-2, -3, __k] = field[-6, -2, __k]
+                    field[-2, -2, __k] = field[-7, -2, __k]
 
-                    field[-3, -4, k] = field[-5, -3, k]
-                    field[-3, -3, k] = field[-6, -3, k]
-                    field[-3, -2, k] = field[-7, -3, k]
+                    field[-3, -4, __k] = field[-5, -3, __k]
+                    field[-3, -3, __k] = field[-6, -3, __k]
+                    field[-3, -2, __k] = field[-7, -3, __k]
 
-                    field[-4, -4, k] = field[-5, -4, k]
-                    field[-4, -3, k] = field[-6, -4, k]
-                    field[-4, -2, k] = field[-7, -4, k]
+                    field[-4, -4, __k] = field[-5, -4, __k]
+                    field[-4, -3, __k] = field[-6, -4, __k]
+                    field[-4, -2, __k] = field[-7, -4, __k]
